6bbc593b26a5b74e56061b87ffe333057b63feb2
muhammadusman2228 <usmanbhatti504021@gmail.com>
2026-02-01 10:02:37 +0500

    writing code for layout

--- app/src/main/java/com/example/mediaplayerassignement/MainActivity.java
+++ app/src/main/java/com/example/mediaplayerassignement/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.mediaplayerassignement;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+Button play,pause;
+MediaPlayer player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,6 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+//        player=MediaPlayer.create(this,R.)
     }
 }
